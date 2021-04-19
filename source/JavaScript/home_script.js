63499f2e052deb5b9df111237db0b127609b1ff3
salterdatwit <54985588+salterdatwit@users.noreply.github.com>
2021-04-18 20:06:55 -0400

    adjustable table size for panel
    table now has ability to have varying number of items per row when panel is opened or closed

--- source/JavaScript/home_script.js
+++ source/JavaScript/home_script.js
@@ -8,29 +8,8 @@ $(document).ready(function () {
 		success: function (response)
 		{
 			console.log("Data Received");
-			var table = $("#pictable");
-			var row = 0;
-
-			for (var count = 0; count < response.length; count++)
-			{
-				var data = response[count];
-
-				if (count % 3 == 0)
-				{
-					table.append("<tr id='row-" + row + "'></tr>");
-				}
-
-				//Adds image to table and data to image
-				$("#row-" + row).append("<td><img class='imgs' src='../images/Full/" + data.link + "'"
-					+ " alt = '" + data.title + "'data-id ='" + data.id + "' data-artist='" + data.artist + "' data-desc='"
-					+ data.desc + "' data-price='" + data.price + "' data-cart='" + data.cart + "'/></td>"
-				);
-
-				if (count % 3 == 2)
-				{
-					row++;
-				}
-			}
+			var list = createImages(response);
+			organizeImages(list, 3);
 			applyPanel();
 		},
 		error: function (response)
@@ -41,7 +20,10 @@ $(document).ready(function () {
 	});
 });
 
-function applyPanel() {
+
+
+function applyPanel()
+{
 	//store all images and create image element for each image in html page.
 	var images = document.getElementsByClassName("imgs");
 	var panel = document.getElementById("panel");
@@ -50,7 +32,7 @@ function applyPanel() {
 	close.onclick = function ()
 	{
 		panel.innerHTML = "";
-		//add reformat picture table function here
+		organizeImages(toArray(document.getElementsByClassName("imgs")), 3);
 	}
 
 	for (var x = 0; x < images.length; x++)
@@ -59,12 +41,93 @@ function applyPanel() {
 	}
 }
 
+function createImages(response)
+{
+	var list = [];
+
+	for (var count = 0; count < response.length; count++)
+	{
+		var data = response[count];
+		var td = document.createElement("td");
+
+		$(td).append("<img class='imgs' src='../images/Full/" + data.link 
+			+ "' alt = '" + data.title + "' data-id ='" + data.id + "' data-artist='" + data.artist + "' data-desc='"
+			+ data.desc + "' data-price='" + data.price + "' data-cart='" + data.cart + "'/>"
+		);
+		list.push(td);		
+	}
+	return list;
+}
+
+function organizeImages(response, rows)
+{
+	console.log("Entering organizeImages,", response, rows);
+
+	var table = $("#pictable");
+	var row = 0;
+
+	for (var count = 0; count < response.length; count++)
+	{
+		if (count % rows == 0 && document.getElementById("row-" + count) == null)
+		{
+			table.append("<tr id='row-" + row + "'></tr>");
+		}
+
+		if (document.getElementById("item-" + count) == null)
+		{
+			$("#row-" + row).append("<td id='item-" + count + "'></td>");
+		}
+		else
+		{
+			$("#row-" + row).append($("#item-" + count));
+		}
+
+		$("#item-" + count).append(response[count]);
+
+		console.log(document.getElementById("row-" + row), count, row, response.length);
+
+		if (count % rows == rows - 1)
+		{
+			row++;
+		}
+	}
+}
+
+function toArray(list)
+{
+	var array = new Array();
+	
+	for (var count = 0; count < list.length; count++)
+	{
+		var data = list[count];
+		var title = data.getAttribute("data-title");
+		var link = data.getAttribute("src").substring(String.prototype.indexOf(data.getAttribute("src")));
+		var id = data.getAttribute("data-id");
+		var desc = data.getAttribute("data-desc");
+		var artist = data.getAttribute("data-artist");
+		var price = data.getAttribute("data-price");
+		var cart = data.getAttribute("data-cart");
+
+		array[count] = {
+			title: title,
+			link: link,
+			id: id,
+			desc: desc,
+			artist: artist,
+			price: price,
+			cart: cart
+		}
+	}
+	return array;
+}
+
 function onClickApplication(orgImage, panel, close)
 {
 	var image = orgImage.cloneNode(false);
 	orgImage.onclick = function ()
 	{
 		panel.innerHTML = "";
+		organizeImages(toArray(document.getElementsByClassName("imgs")), 2);
 		panel.append(close);
 		$(panel).append("</br>");
 		panel.append(image);
@@ -95,6 +158,8 @@ function onClickApplication(orgImage, panel, close)
 
 		add.onclick = function ()
 		{
+			console.log(document.getElementsByClassName("imgs"));
+
 			$.ajax({
 				url: "../php/add_to_cart.php",
 				type: "POST",
@@ -104,12 +169,10 @@ function onClickApplication(orgImage, panel, close)
 				},
 				success: function ()
 				{
-					console.log("Success with AJAX call");
-					console.log($(image).attr("data-cart").toString());
-					$(image).attr("data-cart", "1");
-					console.log($(image).attr("data-cart").toString());
-					$(add).replaceWith(remove);
 
+					console.log("Success with addition AJAX call");
+					$(image).attr("data-cart", "1");
+					$(add).replaceWith(remove);
 				},
 				error: function ()
 				{
@@ -120,6 +183,8 @@ function onClickApplication(orgImage, panel, close)
 		remove.onclick = function ()
 		{
 			console.log("Remove Function Initiated")
+			console.log(document.getElementsByClassName("imgs"));
+
 			$.ajax({
 				url: "../php/remove_item.php",
 				type: "POST",
@@ -129,12 +194,8 @@ function onClickApplication(orgImage, panel, close)
 				},
 				success: function ()
 				{
-					
-					console.log("Success with AJAX call");
-					console.log(parseInt($(image).attr("data-id")));
-					console.log($(image).attr("data-cart").toString());
+					console.log("Success with removal AJAX call");
 					$(image).attr("data-cart", "0");
-					console.log($(image).attr("data-cart").toString());
 					$(remove).replaceWith(add);
 				},
 				error: function ()
